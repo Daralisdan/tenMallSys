f1d9e1e778a47d698c96273fcad17a30175525ff
姚丹 <3034008462@qq.com>
2019-11-20 18:09:16 +0800

    提交代码，完成模板管理模块的功能

--- entity/src/main/java/com/cn/wanxi/entity/template/TemplateEntity.java
+++ entity/src/main/java/com/cn/wanxi/entity/template/TemplateEntity.java
@@ -4,4 +4,41 @@ package com.cn.wanxi.entity.template;
  * 2019/11/18,Create by yaodan
  */
 public class TemplateEntity {
+
+    private int id;
+    private String name;
+    private int paraNum;
+    private int sepcNum;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParaNum() {
+        return paraNum;
+    }
+
+    public void setParaNum(int paraNum) {
+        this.paraNum = paraNum;
+    }
+
+    public int getSepcNum() {
+        return sepcNum;
+    }
+
+    public void setSepcNum(int sepcNum) {
+        this.sepcNum = sepcNum;
+    }
 }
---
+++ service/src/main/java/com/cn/wanxi/service/template/impl/ParaServiceImpl.java
@@ -0,0 +1,46 @@
+package com.cn.wanxi.service.template.impl;
+
+import com.cn.wanxi.dao.template.ParaDao;
+import com.cn.wanxi.entity.template.ParaEntity;
+import com.cn.wanxi.service.template.IParaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: SSJ
+ * @Date: 11月19日 16:23
+ */
+@Service
+public class ParaServiceImpl implements IParaService {
+
+    @Autowired
+    private ParaDao paraDao;
+
+    @Override
+    public int deleteById(int id) {
+        return paraDao.delete(id);
+    }
+
+    @Override
+    public int add(ParaEntity paraEntity) {
+        return paraDao.add(paraEntity);
+    }
+
+    @Override
+    public int update(ParaEntity paraEntity) {
+        return paraDao.update(paraEntity);
+    }
+
+    @Override
+    public List<Map<String, Object>> findAll() {
+        return paraDao.findAll();
+    }
+
+    @Override
+    public Map<String, Object> find(ParaEntity paraEntity, Integer page, Integer size) {
+        return paraDao.find(paraEntity,page,size);
+    }
+}
