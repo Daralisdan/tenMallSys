9987f8dbf8963501e54e040d2cb60fa3d5362a20
rains2711 <271102154@qq.com>
2019-11-25 16:30:11 +0800

    修改bug,合并代码lxh

--- dao/src/main/java/com/cn/wanxi/dao/roleMenu/impl/RoleMenuDaoImpl.java
+++ dao/src/main/java/com/cn/wanxi/dao/roleMenu/impl/RoleMenuDaoImpl.java
@@ -48,7 +48,7 @@ public class RoleMenuDaoImpl implements RoleMenuDao {
         int[] c= null;
         for (String i : split){
             int menids = Integer.parseInt(i);
-            sql =" INSERT INTO wx_tab_rolemenu ( role_id, menu_id) VALUES ("+ids+" , "+menids+")";
+            sql =" INSERT INTO wx_tab_roleMenu ( role_id, menu_id) VALUES ("+ids+" , "+menids+")";
             c = jdbcTemplate.batchUpdate(sql);
             System.out.println(c.length);
         }
@@ -59,7 +59,7 @@ public class RoleMenuDaoImpl implements RoleMenuDao {
     @Override
     public RoleMenuEntity findByRoleId(int roleid) {
         RoleMenuEntity roleMenuEntity = null;
-        String exeSQL = "select id, role_id as roleId, menu_id as menuId from wx_tab_rolemenu where role_id=?";
+        String exeSQL = "select id, role_id as roleId, menu_id as menuId from wx_tab_roleMenu where role_id=?";
         List<RoleMenuEntity> roleMenuEntities = jdbcTemplate.query(exeSQL, new Object[]{roleid}, new BeanPropertyRowMapper<RoleMenuEntity>(RoleMenuEntity.class));
         if (null != roleMenuEntities && roleMenuEntities.size() > 0) {
             roleMenuEntity = roleMenuEntities.get(0);
@@ -75,7 +75,7 @@ public class RoleMenuDaoImpl implements RoleMenuDao {
         int[] c= null;
         for (String i : split){
             int menids = Integer.parseInt(i);
-            sql =" delete  from wx_tab_rolemenu where role_id= "+ids+" and menu_id= "+menids+" ";
+            sql =" delete  from wx_tab_roleMenu where role_id= "+ids+" and menu_id= "+menids+" ";
             c = jdbcTemplate.batchUpdate(sql);
             System.out.println(c.length);
         }
