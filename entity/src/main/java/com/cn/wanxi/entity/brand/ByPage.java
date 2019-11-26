f9c7c1dd6b3b9aa825dfd9d87bbdaca8d6b9aafd
姚丹 <3034008462@qq.com>
2019-11-26 20:42:49 +0800

    解决冲突

---
+++ entity/src/main/java/com/cn/wanxi/entity/brand/ByPage.java
@@ -0,0 +1,15 @@
+package com.cn.wanxi.entity.brand;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * 2019/11/24,Create by yaodan
+ */
+@Data
+public class ByPage {
+    private Map<String,Object> searchMap;
+    private int page;
+    private int size;
+}
